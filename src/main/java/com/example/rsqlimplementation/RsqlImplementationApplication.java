b2cd8984e5282bcac04d82d061e425a45ac7c7f6
n.zhiznevskiy <n.zhiznevskii@itfbgroup.ru>
2023-11-28 16:08:25 +0300

    Initial commit

---
+++ src/main/java/com/example/rsqlimplementation/RsqlImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsqlimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsqlImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsqlImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rsqlimplementation/RsqlImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.rsqlimplementation;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RsqlImplementationApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
